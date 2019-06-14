d48312435eb12335a0b9544f49167ae8e7842e51
amberm <nekosenpai@gmail.com>
2019-06-14 09:33:31 -0600

    separating out the info

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,6 +8,24 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  printf("%d command line args: \n", argc);
+
+  for (int i = 0; i < argc; i++) {
+    printf("   %s\n", argv[i]);
+  }
+
+  // for (int j = 0; j < argv; j++) {
+  //   printf(" %s/n", argv[j]);
+  // }
+
+  char *stringy;
+
+  if (argv < 2) {
+    stringy = ".";
+  }
+
+  
+
   // Open directory
 
   // Repeatly read and print entries
